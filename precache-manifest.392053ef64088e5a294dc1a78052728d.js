84a0060d9c8b0aadb33edb16e9fa32de349f0ede
mgame99 <mansur.1999@bk.ru>
2021-12-04 23:23:21 +0500

    deploy

---
+++ precache-manifest.392053ef64088e5a294dc1a78052728d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733d61a5cc7843230e70",
+    "url": "/pair/css/app.ece469a1.css"
+  },
+  {
+    "revision": "262da9cc41c267c4180f3c5d75202445",
+    "url": "/pair/fonts/Comfortaa-Bold.262da9cc.woff"
+  },
+  {
+    "revision": "416748edc47b846fed63b34428119cbf",
+    "url": "/pair/fonts/Comfortaa-Bold.416748ed.woff2"
+  },
+  {
+    "revision": "801965b0436842e843141a5a6c1e387f",
+    "url": "/pair/fonts/Comfortaa-Bold.801965b0.ttf"
+  },
+  {
+    "revision": "14f89c9688137c5257b7a4f129097da4",
+    "url": "/pair/fonts/Comfortaa-Regular.14f89c96.woff2"
+  },
+  {
+    "revision": "9d2963abb973811ae0f5ed64c1a5ab18",
+    "url": "/pair/fonts/Comfortaa-Regular.9d2963ab.woff"
+  },
+  {
+    "revision": "c2488bfda16e6a811f62414da90e51e4",
+    "url": "/pair/fonts/Comfortaa-Regular.c2488bfd.ttf"
+  },
+  {
+    "revision": "2d2e2c7aa852df4981d65b3b61293a92",
+    "url": "/pair/img/auto-size.2d2e2c7a.svg"
+  },
+  {
+    "revision": "834fef031ac2610fd82a073764d0ed16",
+    "url": "/pair/img/full-size.834fef03.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pair/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8ca4b93ca809ee3ecf05370b13a9228a",
+    "url": "/pair/img/moon.8ca4b93c.svg"
+  },
+  {
+    "revision": "b9113c6473e115527d6a6d6aadacb2ea",
+    "url": "/pair/img/sun.b9113c64.svg"
+  },
+  {
+    "revision": "fc6b29cf0fa206be2430fb24961cf658",
+    "url": "/pair/index.html"
+  },
+  {
+    "revision": "733d61a5cc7843230e70",
+    "url": "/pair/js/app.5ecc5b7d.js"
+  },
+  {
+    "revision": "59a6aa2094e745d7b5df",
+    "url": "/pair/js/chunk-vendors.d893b11e.js"
+  },
+  {
+    "revision": "57207c04f95927b0e0165dbec50e77b3",
+    "url": "/pair/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pair/robots.txt"
+  }
+]);
