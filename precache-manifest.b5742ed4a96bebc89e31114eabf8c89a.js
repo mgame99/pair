2a48c1cf7d97d5d7c105b3ec0602668f19e9347e
Mansurkhoja <mansur.atm@gmail.com>
2022-07-02 00:31:58 +0500

    deploy

---
+++ precache-manifest.b5742ed4a96bebc89e31114eabf8c89a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67004d633bab45351af",
+    "url": "/pair/css/app.936a98c5.css"
+  },
+  {
+    "revision": "5c2f24e7fbbed9424f695a3f1f2339af",
+    "url": "/pair/favicon/android-chrome-192x192.png"
+  },
+  {
+    "revision": "86d3bf3075dca64ce7f481684c42721e",
+    "url": "/pair/favicon/android-chrome-512x512.png"
+  },
+  {
+    "revision": "2fafad4a108748eb51ac6fd499cd780a",
+    "url": "/pair/favicon/apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/pair/favicon/browserconfig.xml"
+  },
+  {
+    "revision": "ad69f042865a4b0bd56cdec14d93c8ff",
+    "url": "/pair/favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "bb547bed19ffa9872fc686195e92164e",
+    "url": "/pair/favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "5c7a76f6375d583f9d453924c1241b89",
+    "url": "/pair/favicon/mstile-150x150.png"
+  },
+  {
+    "revision": "db25e3eaf3208c856794283fcb69fbbd",
+    "url": "/pair/favicon/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "1de94ab7d8b08c706ee7afb2a2f8b749",
+    "url": "/pair/favicon/site.webmanifest"
+  },
+  {
+    "revision": "262da9cc41c267c4180f3c5d75202445",
+    "url": "/pair/fonts/Comfortaa-Bold.262da9cc.woff"
+  },
+  {
+    "revision": "416748edc47b846fed63b34428119cbf",
+    "url": "/pair/fonts/Comfortaa-Bold.416748ed.woff2"
+  },
+  {
+    "revision": "801965b0436842e843141a5a6c1e387f",
+    "url": "/pair/fonts/Comfortaa-Bold.801965b0.ttf"
+  },
+  {
+    "revision": "14f89c9688137c5257b7a4f129097da4",
+    "url": "/pair/fonts/Comfortaa-Regular.14f89c96.woff2"
+  },
+  {
+    "revision": "9d2963abb973811ae0f5ed64c1a5ab18",
+    "url": "/pair/fonts/Comfortaa-Regular.9d2963ab.woff"
+  },
+  {
+    "revision": "c2488bfda16e6a811f62414da90e51e4",
+    "url": "/pair/fonts/Comfortaa-Regular.c2488bfd.ttf"
+  },
+  {
+    "revision": "2d2e2c7aa852df4981d65b3b61293a92",
+    "url": "/pair/img/auto-size.2d2e2c7a.svg"
+  },
+  {
+    "revision": "8f476f7888703ffc2b5478778572bc75",
+    "url": "/pair/img/en.8f476f78.svg"
+  },
+  {
+    "revision": "834fef031ac2610fd82a073764d0ed16",
+    "url": "/pair/img/full-size.834fef03.svg"
+  },
+  {
+    "revision": "3f7e054c96b91cbd52a02d4840cf2e60",
+    "url": "/pair/img/logo.3f7e054c.svg"
+  },
+  {
+    "revision": "7feb8e3ac19a3fc3e2cb15fd98f5c8f3",
+    "url": "/pair/img/moon.7feb8e3a.svg"
+  },
+  {
+    "revision": "e84818c5cb35a605cd272dbe88ded65b",
+    "url": "/pair/img/ru.e84818c5.svg"
+  },
+  {
+    "revision": "b9113c6473e115527d6a6d6aadacb2ea",
+    "url": "/pair/img/sun.b9113c64.svg"
+  },
+  {
+    "revision": "6bce870b6f55c848986411762b61add6",
+    "url": "/pair/index.html"
+  },
+  {
+    "revision": "d67004d633bab45351af",
+    "url": "/pair/js/app.a9e18feb.js"
+  },
+  {
+    "revision": "464c01c65af6779fccb7",
+    "url": "/pair/js/chunk-vendors.7e5451d4.js"
+  },
+  {
+    "revision": "57207c04f95927b0e0165dbec50e77b3",
+    "url": "/pair/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pair/robots.txt"
+  }
+]);
